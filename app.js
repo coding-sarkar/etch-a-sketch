f3a6f40274415dd94bb904b1a32277e11ab3e6ac
Souvik Dey Sarkar <sdeys2111@gmail.com>
2022-04-26 23:45:42 +0530

    Add function to reset grid color

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 let gridSize = 16;
 const container = document.querySelector(".container");
+const resetButton = document.querySelector("button");
+
+resetButton.addEventListener("click", gridReset);
+
+function gridReset() {
+    const gridBoxes = Array.from(container.querySelectorAll(".grid-box"));
+    gridBoxes.forEach((box) => {
+        box.style.backgroundColor = "white";
+    });
+}
+
+function mouseHover(e) {
+    e.target.style.backgroundColor = "red";
+}
 
 for (let i = 0; i < (gridSize**2); i++){
     const gridBox = document.createElement("div");
+    
     gridBox.classList.add("grid-box");
+    
+    // divides total container width by number of rows to get evenly spaced grid boxes
     gridBox.style.flexBasis = `calc(100% / ${gridSize})`;
+
+    gridBox.addEventListener("mouseover", mouseHover, {once: true});
+
     container.appendChild(gridBox);
 }
